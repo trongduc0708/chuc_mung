ecb6100f769e0b953931f1d06b67234eed4cc503
trongduc0708 <63398745+trongduc0708@users.noreply.github.com>
2023-09-29 17:12:12 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = '#';
+const eventName = 'Chúc mừng Trung thu';
+const titleCard = 'Gửi Tặng Em';
+const contentCard = 'May the moonlit night bring you luck and blessings that last throughout the year. Happy Mid-Autumn Festival!';
+
+const giftImage = 'ava2.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
